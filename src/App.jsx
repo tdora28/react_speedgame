4e45b31852adbd61eafda6c87fc2be52cf21005a
Dóra Tokai <tokaidora88@gmail.com>
2023-11-28 14:39:54 +0200

    Add GameOver component and switch for game states

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,40 @@ import { useState } from "react";
 import { levels } from "./levels.js";
 import NewGame from "./components/NewGame.jsx";
 import Game from "./components/Game.jsx";
+import GameOver from "./components/GameOver.jsx";
 
 function App() {
   const [player, setPlayer] = useState();
   const [circles, setCircles] = useState([]);
   const [score, setScore] = useState(0);
+  const [gameLaunch, setGameLaunch] = useState(true);
+  const [gameOn, setGameOn] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
 
   const gameSetHandler = (level, name) => {
     const levelIndex = levels.findIndex((el) => el.name === level);
     const levelAmount = levels[levelIndex].amount;
-
     const circlesArray = Array.from({ length: levelAmount }, (x, i) => i);
-
     setCircles(circlesArray);
-
     setPlayer({
       level: level,
       name: name,
     });
+    setGameLaunch(!gameLaunch);
+    setGameOn(!gameOn);
+  };
+
+  const stopHandler = () => {
+    setGameOn(!gameOn);
+    setGameOver(!gameOver);
   };
 
   return (
     <>
       <h1>Speedy Donuts</h1>
-      <NewGame onclick={gameSetHandler} />
-      <Game score={score} circles={circles} />
+      {gameLaunch && <NewGame onclick={gameSetHandler} />}
+      {gameOn && <Game score={score} circles={circles} stopHandler={stopHandler} />}
+      {gameOver && <GameOver />}
     </>
   );
 }
