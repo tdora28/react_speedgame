b4b30b1551088e1d19c0921ec399c0363293363b
Dóra Tokai <tokaidora88@gmail.com>
2023-12-12 17:05:43 +0200

    Add UUID for the circles

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,13 +1,22 @@
 import Circle from "../UI_components/Circle.jsx";
 
 function Game({ score, circles, current, stopHandler, clickHandler, musicHandler }) {
+  const generateUUID = () => {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+      var r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  };
+
   musicHandler("play", "bg");
+
   return (
     <div className="layout-game">
       <p className="layout-game__score">Score: {score}</p>
       <div className="circles">
         {circles.map((el, i) => (
-          <Circle key={`${i}${Math.random()}`} id={i} current={current === i} clickHandler={clickHandler} />
+          <Circle key={generateUUID()} id={i} current={current === i} clickHandler={clickHandler} />
         ))}
       </div>
       <button className="button" onClick={stopHandler}>
